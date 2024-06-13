3a485051129f0c2acd3c4afe0783ac9d63eb4ad3
Stephen Sure <stephan.sure@gmail.com>
2024-06-13 07:32:27 +0000

    Add sinup signin form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://sure-moments-8fa30987b0d1.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
